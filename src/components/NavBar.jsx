908fa6ade72a8dcaf756de1b8f3526eb84a94de7
dilshan <dilshan.maduranga@itelasoft.com>
2024-05-13 00:27:28 +0530

    some changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/services">Services</a></li>
-        <li><a href="/services">Services</a></li>
+        <li><a href="/products">Items</a></li>
         <li><a href="/contact">Contact</a></li>
         <li><a href="/index">Login</a></li>
       </ul>
